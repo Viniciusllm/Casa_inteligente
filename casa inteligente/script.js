90e308c711f4387c738b93c285ce7843a2f7c0bd
Vini <viny20.bm2016@gmail.com>
2025-11-25 11:51:33 -0300

    Atualizando o console.log e mudando o nome com o innert HTML finalizar em casa

--- casa inteligente/script.js
+++ casa inteligente/script.js
@@ -5,40 +5,125 @@ const MQTT_URL = "wss://test.mosquitto.org:8081";
 const clienteId = "web-casa-inteligente-vinicius";
 
 // ### Criando o cliente MQTT do navegador, POrem ainda não esamos conectados ao BROKER
-const cliente = mqtt.connect(MQTT_URL, {
+// const cliente = mqtt.connect(MQTT_URL, {
 
-    clienteId,
-    clean: true,
-    connectTimeout: 4000,
-})
+//     clienteId,
+//     clean: true,
+//     connectTimeout: 4000,
+// })
+
+// #########  Variaveis dos dispositivos
+const lampSalaInput = document.getElementById("lamp-sala");
+const lampSalaTexto = document.getElementById("lamp-sala-texto");
+
+const cortSalaInput = document.getElementById("cort-sala");
+const cortSalaTexto = document.getElementById("cort-sala-texto");
+
+const portSalaInput = document.getElementById("port-sala")
+const portSalaTexto = document.getElementById("port-sala-texto")
+
+const lampCozinhaInput = document.getElementById("lamp-cozinha");
+const lampCozinhaTexto = document.getElementById("lamp-cozinha-texto");
+
+const exausCozinhaInput = document.getElementById("exaus-cozinha");
+const exausCozinhaTexto = document.getElementById("exaus-cozinha-texto");
+
+
+// ########## Alterando estado dos dispositivos
+// pegando o evento de mudança do botão (ligado/desligado)
+lampSalaInput.addEventListener("change", () => {
+
+    // verifica se o botao esta como ligado
+    const ligado = lampSalaInput.checked === true;
+
+    if(ligado === true){
+        console.log("Lampada Ligada");
+        lampSalaTexto.innerHTML = "On"
+    } else {
+        console.log("Lampada Desligada");
+        lampSalaTexto.innerHTML = "Off"
+    }
+});
+
+cortSalaInput.addEventListener("change",() => {
+    const aberta = cortSalaInput.checked === true;
+
+    if(aberta === true){
+        console.log("Cortina Ligada");
+        cortSalaTexto.innerHTML = "Open"
+    } else {
+        console.log("Cortina Fechada");
+        cortSalaTexto.innerHTML = "Closer"
+    }
+
+});
+
+portSalaInput.addEventListener("change", () => {
+    const aberta = portSalaInput.checked === true;
+
+    if(aberta === true){
+        console.log("Porta Aberta");
+        portSalaTexto.innerHTML = "Open"
+    } else {
+        console.log("Porta Fechada");
+        portSalaTexto.innerHTML = "Closer"
+    }
+});
+
+lampCozinhaInput.addEventListener("change", () => {
+
+    const ligada = lampCozinhaInput.checked === true;
+
+    if(ligada === true){
+        console.log("Lampada Ligada");
+        lampCozinhaTexto.innerHTML = "On"
+    } else {
+        console.log("Lampada desligada");
+        lampCozinhaTexto.innerHTML = "Off"
+    }
+});
+
+exausCozinhaInput.addEventListener("change", () => {
+    const ligado = exausCozinhaInput.checked === true;
+
+    if(ligado === true){
+        console.log("Exaustor Ligado");
+        exausCozinhaTexto.innerHTML = "On"
+    }else {
+        console.log("Exaustor Desligado");
+        exausCozinhaTexto.innerHTML = "Off"
+    }
+});
+
+        
 
 // #### Chamando um evento do JS do tipo "DOMContentLoaded", que é o evento que acontece após toda a minha página de HTML ser carregada
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Página carregada com sucesso!👁️👄👁️ ... conectando ao Mosquitto!");
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log("Página carregada com sucesso!👁️👄👁️ ... conectando ao Mosquitto!");
 
-    // ##### Estabelecendo a conexão com o broker mqtt
-    cliente.on("connect", () => {
-        console.log("Conexão estabelecida com Sucesso!✅");
-        console.log("Cliente conectado:", clienteId);
+//     // ##### Estabelecendo a conexão com o broker mqtt
+//     cliente.on("connect", () => {
+//         console.log("Conexão estabelecida com Sucesso!✅");
+//         console.log("Cliente conectado:", clienteId);
 
-    // ######## criando um topico para acessar mensagens do MQTT
-        const topicoTeste = 'teste/Vinicius';
+//     // ######## criando um topico para acessar mensagens do MQTT
+//         const topicoTeste = 'teste/Vinicius';
 
-    //  ######## Recebendo mensagens do topico criado  
-        cliente.subscribe(topicoTeste);
+//     //  ######## Recebendo mensagens do topico criado  
+//         cliente.subscribe(topicoTeste);
 
 
-    });
+//     });
 
-    // ###### Preparando mensaem de erro caso algo aconteça
-    cliente.on("error", () => {
-        console.error("Erro ao conectar ao Broker MQTT!💀");
-        console.error(erro);
-    });
+//     // ###### Preparando mensaem de erro caso algo aconteça
+//     cliente.on("error", () => {
+//         console.error("Erro ao conectar ao Broker MQTT!💀");
+//         console.error(erro);
+//     });
 
-    // ####### Recebendo as mensagens dos tópicos assinados no MQTT pelo cliente 
-    cliente.on("message", (topico, mensagem) => {
-        console.log("Topico recebido: ", topico);
-        console.log("Mensagem recebida: ", mensagem);
-    })
-})
+//     // ####### Recebendo as mensagens dos tópicos assinados no MQTT pelo cliente 
+//     cliente.on("message", (topico, mensagem) => {
+//         console.log("Topico recebido: ", topico);
+//         console.log("Mensagem recebida: ", mensagem);
+//     })
+// })
